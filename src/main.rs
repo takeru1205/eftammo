91d8b446737d9b533d7c2a68654ce68ba1468a3c
takeru1205 <takeru0639418@gmail.com>
2023-02-15 00:08:26 +0900

    [update] calc levenshtein similarity between 7.62x39 BP to all ammo name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate csv;
+extern crate levenshtein;
 
 use csv::StringRecord;
+use levenshtein::levenshtein;
 use std::env;
 use std::error::Error;
 use std::ffi::OsString;
@@ -23,14 +25,18 @@ fn csv_parse(
     file: File,
     name_vec: &mut Vec<String>,
     ammo_vec: &mut Vec<StringRecord>,
+    similarity_vec: &mut Vec<usize>,
 ) -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(file);
+    // let ammo = String::from("7.62x39 BP");
+    let ammo = "7.62x39 BP";
     for result in rdr.records() {
         let record = result?;
         println!("{:?}", record);
         ammo_vec.push(record.clone());
         if let Some(name) = record.get(0) {
             name_vec.push(name.to_string());
+            similarity_vec.push(levenshtein(ammo, name));
         }
     }
     Ok(())
@@ -47,8 +53,14 @@ fn main() {
 
     let mut name_vec: Vec<String> = Vec::new();
     let mut ammo_vec: Vec<StringRecord> = Vec::new();
+    let mut similarity_vec: Vec<usize> = Vec::new();
 
-    match csv_parse(fopen(&file_path).unwrap(), &mut name_vec, &mut ammo_vec) {
+    match csv_parse(
+        fopen(&file_path).unwrap(),
+        &mut name_vec,
+        &mut ammo_vec,
+        &mut similarity_vec,
+    ) {
         Ok(_) => println!("Successfully parsed CSV"),
         Err(err) => {
             eprintln!("Error: {}", err);
@@ -57,6 +69,8 @@ fn main() {
     }
     println!("{:?}", name_vec);
     println!("{:?}", ammo_vec);
+    println!("{:?}", similarity_vec);
     println!("length of name_vec is {}", name_vec.len());
     println!("length of ammo_vec is {}", ammo_vec.len());
+    println!("length of similarity_vec is {}", similarity_vec.len());
 }
